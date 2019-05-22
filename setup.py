6d484c0989d6a29b7016ce8b951ce41d0fd76232
Taylor Denouden <taylordenouden@gmail.com>
2019-05-22 17:10:51 -0400

    Add __version__ to package

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import ood_metrics
 
-version = '0.2.1'
-
-with open('./README.md') as f:
-    long_description = f.read()
 
 setup(
     name='ood-metrics',
-    version=version,
+    version=ood_metrics.__version__,
     author='Taylor Denouden',
     author_email='taylordenouden@gmail.com',
     packages=find_packages(),
     url='https://github.com/tayden/ood-metrics',
     license='MIT',
     description='Calculate common OOD detection metrics',
-    long_description=long_description,
+    long_description=open('./README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=[
         "numpy>=1.15.3",
